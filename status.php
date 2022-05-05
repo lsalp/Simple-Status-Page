4a65045f26aed7df22bd796a23271ae71c96b587
Joseph Helela <josephh@yatosha.com>
2022-05-05 08:06:41 +0300

    Update status.php
    Auto Page refresh

--- status.php
+++ status.php
@@ -2,6 +2,7 @@
 <html lang="en" class="h-100">
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="refresh" content="10">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Status Page · Server Monitor</title>
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
